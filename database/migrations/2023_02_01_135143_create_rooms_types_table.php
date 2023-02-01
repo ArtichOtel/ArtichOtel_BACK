b473b190f5553dc2cd9c42c8609c0fd6367fde3d
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-01 15:07:56 +0100

    Models Offer, Advantage, New, Video, RoomsType

---
+++ database/migrations/2023_02_01_135143_create_rooms_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->string('description', 300);
+            $table->string('url_image', 255);
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms_types');
+    }
+};
