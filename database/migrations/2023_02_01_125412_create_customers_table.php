f91f9fe12bd19d5ce91c3357294bdce0c610423b
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-01 13:56:46 +0100

    make customers migration

---
+++ database/migrations/2023_02_01_125412_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name',45);
+            $table->string('last_name',45);
+            $table->string('phone_number',10);
+            $table->string('lang',10);
+            $table->string('avatar_url',255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
