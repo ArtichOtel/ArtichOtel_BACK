de793f65b3fb308af812c765198ed389573b32b5
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-02-02 13:14:48 +0100

    add doctrine, models : customer,role,review,user

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
+class Role extends Model
 {
     use HasFactory;
 
-    function customer() {
-        return $this->belongsTo(User::class);
+    function user() {
+        return $this->belongsToMany(User::class);
     }
 }
