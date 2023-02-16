896e618d20bc5e55fc6e033d400cf7a85ea0fe77
Anthony26100 <anthony.bellier@laposte.net>
2023-02-16 14:36:05 +0100

    orderby footer query

--- app/Http/Controllers/API/Footer/FooterController.php
+++ app/Http/Controllers/API/Footer/FooterController.php
@@ -26,9 +26,10 @@ class FooterController extends Controller
         // $footers = Footer::all();
 
         $urls = Footer::query()
-            ->select('footers.title', 'footers.order', 'footer_link.id', 'links.url', 'links.text', 'links.icon')
+            ->select('footers.id', 'footers.title', 'footers.order', 'links.url', 'links.text', 'links.icon')
             ->join('footer_link', 'footers.id', '=', 'footer_link.footer_id')
             ->join('links', 'links.id', '=', 'footer_link.link_id')
+            ->orderBy('footers.order')
             ->get();
 
 
