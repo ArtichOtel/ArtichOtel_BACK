4bd63a730d9ccbb81d3fceb2ac04cdfd4aab1899
Anthony26100 <anthony.bellier@laposte.net>
2023-02-24 14:18:28 +0100

    (test) use Customer role & test api route forbiden for customer

--- tests/Feature/Utils/UserConnexion.php
+++ tests/Feature/Utils/UserConnexion.php
@@ -6,7 +6,8 @@ use App\Models\Role;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 
-class UserConnexion {
+class UserConnexion
+{
 
     /**
      * It's getting the abillities of the user
@@ -15,24 +16,42 @@ class UserConnexion {
      * 
      * @return array It's returning the abillities of the user.
      */
-    private static function getUserRoleAbilities(User $user): array {
-         $abillities = Role::query()
-         ->select(['roles.abilities'])
-         ->join('users', 'roles.id', '=', 'users.role_id')
-         ->where('users.id', '=', $user['id'])
-         ->get()[0]["abilities"];
+    private static function getUserRoleAbilities(User $user): array
+    {
+        $abillities = Role::query()
+            ->select(['roles.abilities'])
+            ->join('users', 'roles.id', '=', 'users.role_id')
+            ->where('users.id', '=', $user['id'])
+            ->get()[0]["abilities"];
 
-         return $abillities;
+        return $abillities;
     }
 
     /**
      * Acting as the user 'Admin' with its abillity.
      */
-    public static function fakeAdmin(): void {
+    public static function fakeAdmin(): void
+    {
         /* It's getting the user with the pseudo Admin. */
         $user = User::query()
             ->select(['*'])
             ->where('pseudo', '=', 'Admin')
+            ->get()[0];
+
+        $abillity = self::getUserRoleAbilities($user)[0];
+
+        Sanctum::actingAs($user, [$abillity]);
+    }
+
+    /**
+     * Acting as the user 'Admin' with its abillity.
+     */
+    public static function fakeUser(): void
+    {
+        /* It's getting the user with the pseudo Admin. */
+        $user = User::query()
+            ->select(['*'])
+            ->where('role_id', '=', 2)
             ->get()[0];
 
         $abillity = self::getUserRoleAbilities($user)[0];
