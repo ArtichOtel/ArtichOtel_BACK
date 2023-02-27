152a884bbeed4781964a5e4456f39fdb081617a1
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-02-27 11:31:40 +0100

    Création factory Adresses ? #16

---
+++ database/factories/AdresseFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Adresse>
+ */
+class AdresseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'country' => fake()->country(),
+            'zipcode' => fake()->countryCode(),
+            'city' => fake()->city(),
+            'address' => fake()->streetAddress(),
+            'additional' => 'apt. ' . rand(1, 30),
+            'is_pro' => rand(1),
+            'customer_id' => fake()->randomDigitNotZero()
+        ];
+    }
+}
