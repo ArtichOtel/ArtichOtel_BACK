43467bf72b11a1fd69d1c58f2ce49fc2b621eee2
Anthony <anthony.bellier@laposte.net>
2023-03-10 11:02:23 +0100

    (feat) add update resquest for booking

---
+++ app/Http/Requests/Booking/BookingUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Booking;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'begin_date' => ['sometimes', 'date'],
+            'end_date' => ['sometimes', 'date'],
+            'status' => ['sometimes', 'string:ascii', 'max:40'],
+            'nbrs_people' => ['sometimes', 'numeric:integer', 'between:1,3'],
+        ];
+    }
+}
